0e081456206e2897452adf1eaf879e4440a21dfd
Nick Wanner <53022718+nickrwann@users.noreply.github.com>
2025-11-18 22:24:21 -0600

    Add dev environment bootstrap files

---
+++ windows/setup-windows.ps1
@@ -0,0 +1,6 @@
+winget install --id Microsoft.WindowsTerminal -e --source winget
+winget install --id Microsoft.VisualStudioCode -e --source winget
+winget install --id Git.Git -e --source winget
+winget install --id Docker.DockerDesktop -e --source winget
+
+wsl --install -d Ubuntu-22.04
